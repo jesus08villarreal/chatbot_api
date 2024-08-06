cbcfe4fb44c8cadb922fc939de11fb1e4d517231
jesusvs1100 <jesusvs1100@gmail.com>
2024-08-06 05:17:19 -0600

    other test

--- test_app.py
+++ test_app.py
@@ -7,6 +7,6 @@ client = TestClient(app)
 
 # Prueba para el endpoint de WhatsApp
 def test_receive_whatsapp():
-    response = client.post("/whatsapp", data={"Body": "Hola, quiero hacer un pedido"})
+    response = client.post("/api/whatsapp", data={"Body": "Hola, quiero hacer un pedido"})
     assert response.status_code == 200
     assert "message" in response.text
