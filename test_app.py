175c7066a4f90d9ba640d8805fa7b4fd010c1eff
jesusvs1100 <jesusvs1100@gmail.com>
2024-08-06 03:33:53 -0600

    test new

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import app
 client = TestClient(app)
 
 def test_whatsapp():
-    name = "Chuy"
-    response = client.post("/helloword", data={"Body": name})
+    name = "Chatbot"
+    response = client.post("/whatsapp", data={"Body": name})
     assert response.status_code == 200
     assert response.json() == {"message": f"Hello {name}"}
